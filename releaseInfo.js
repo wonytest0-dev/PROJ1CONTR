4016ecb23702a0a01a9d7d6e3e643b246a7bcc31
wonytest0-dev <wonytest0@gmail.com>
2026-05-21 10:41:31 +0700

    first commit

---
+++ releaseInfo.js
@@ -0,0 +1,41 @@
+const releaseInfo = {
+
+  "MUSE (WANDER ver.)": {
+    type: "album",
+    image:
+      "https://res.cloudinary.com/ddh2uwdlk/image/upload/…ee6bfad3c7aac357dbb5082_ohnpne.jpg"
+  },
+
+  "MUSE (WONDER ver.)": {
+    type: "album",
+    image:
+      "https://res.cloudinary.com/ddh2uwdlk/image/upload/fl_preserve_transparency/v1779332588/f9ce8a7487f069510360f8d67bbb3961_cmowsy.jpg"
+  },
+
+  "MUSE": {
+    type: "album",
+    image:
+      "https://res.cloudinary.com/ddh2uwdlk/image/upload/fl_preserve_transparency/v1779332589/d55f7491265031e0e729ec007727882f_ezuopb.jpg"
+  },
+
+  "FACE": {
+    type: "album",
+    image:
+      "https://res.cloudinary.com/ddh2uwdlk/image/upload/…60167f5283d41d587939beb_eedotk.jpg"
+  },
+
+  "SINGLES": {
+    type: "single",
+    image:
+      null
+  },
+
+  "ANGEL": {
+    type: "album",
+    image:
+      "https://res.cloudinary.com/ddh2uwdlk/image/upload/…fb681c56ef507967cd6541f_konisj.jpg"
+  }
+};
+
+module.exports =
+  releaseInfo;
---
+++ releaseMap.js
@@ -0,0 +1,67 @@
+const releaseMap = {
+
+  "MUSE (WANDER ver.)": [
+    "Rebirth (Intro)",
+    "Interlude : Showtime",
+    "Smeraldo Garden Marching Band (feat. Loco)",
+    "Slow Dance (feat. Sofia Carson)",
+    "Be Mine",
+    "Who",
+    "Closer Than This",
+    "Who (Instrumental)",
+    "Who (Acoustic Remix)",
+    "Who (Rock Remix)"
+  ],
+
+  "MUSE (WONDER ver.)": [
+    "Rebirth (Intro)",
+    "Interlude : Showtime",
+    "Smeraldo Garden Marching Band (feat. Loco)",
+    "Slow Dance (feat. Sofia Carson)",
+    "Be Mine",
+    "Who",
+    "Closer Than This",
+    "Who (Instrumental)",
+    "Who (Acoustic Remix)",
+    "Who (Rock Remix)"
+  ],
+
+  "MUSE": [
+    "Rebirth (Intro)",
+    "Interlude : Showtime",
+    "Smeraldo Garden Marching Band (feat. Loco)",
+    "Slow Dance (feat. Sofia Carson)",
+    "Be Mine",
+    "Who",
+    "Closer Than This"
+  ],
+
+  "FACE": [
+    "Face-off",
+    "Interlude : Dive",
+    "Like Crazy",
+    "Alone",
+    "Set Me Free Pt.2",
+    "Like Crazy (English Version)"
+  ],
+
+  "SINGLES": [
+    "Promise",
+    "Christmas Love",
+    "VIBE (feat. Jimin of BTS)",
+    "With you",
+    "With you - Instrumental"
+  ],
+
+  "ANGEL": [
+    "Angel Pt. 1 (feat. Kodak Black, NLE Choppa, Jimin of BTS, JVKE & Muni Long)",
+    "Angel Pt. 2 (feat. Jimin of BTS & JVKE feat. Charlie Puth)",
+    "Angel Pt. 1 (feat. Jimin of BTS, JVKE & Muni Long) - Track Version",
+    "Angel Pt. 1 (feat. Jimin of BTS, JVKE & Muni Long) - Sped Up",
+    "Angel Pt. 2 - Acoustic Version",
+    "Angel (feat. Muni Long, JVKE, NLE Choppa) (Anniversary Edition)",
+    "Angel Pt. 2 - Sped Up"
+  ]
+};
+
+module.exports = releaseMap;
