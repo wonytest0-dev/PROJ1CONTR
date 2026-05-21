ca3d5a2fee99ad8d0a333ff0f4128965248bfee3
wonytest0-dev <wonytest0@gmail.com>
2026-05-22 03:27:01 +0700

    Add files via upload

---
+++ save-login.js
@@ -0,0 +1,39 @@
+const { chromium } =
+  require("playwright");
+
+(async () => {
+  const browser =
+    await chromium.launch({
+      headless: false
+    });
+
+  const context =
+    await browser.newContext();
+
+  const page =
+    await context.newPage();
+
+  await page.goto(
+    "https://open.spotify.com/login"
+  );
+
+  console.log(
+    "😭 LOGIN MANUAL DULU"
+  );
+
+  // tunggu kamu login
+  await page.waitForTimeout(
+    120000
+  );
+
+  await context.storageState({
+    path:
+      "spotify-login.json"
+  });
+
+  console.log(
+    "✅ spotify-login.json saved"
+  );
+
+  await browser.close();
+})();
